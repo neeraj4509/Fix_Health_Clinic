98196919ef8cf3b6009143656f3cfb04a9f3f893
neeraj4509 <neerajbendkhale@gmail.com>
2024-01-22 19:19:29 +0530

    update images

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './Testimonials.css';
 
+import image1 from '../assets/images/img1.jpg';
+import image2 from '../assets/images/img2.jpg';
+import image3 from '../assets/images/img3.jpg';
+import image4 from '../assets/images/img4.jpg';
+
+
 function Testimonials() {
   
   const testimonials = [
-    { id: 1, text: "I had a wonderful experience with the services. Highly recommended!", author: "John Doe"  },
-    { id: 2, text: "The professionalism and care I received were top-notch. Thank you!", author: "Jane Smith" },
-    { id: 3, text: "Fantastic service! I felt comfortable and well-informed throughout.", author: "Emily Johnson" },
-    { id:4, text: "Six Month with Fix Health was less than 1 month of my insurance co-pay", author:"Nasir Kamal"}
+    { id: 1, text: "I had a wonderful experience with the services. Highly recommended!", author: "John Doe"  , imageURl : image1,},
+    { id: 2, text: "The professionalism and care I received were top-notch. Thank you!", author: "Jane Smith" , imageURl :image2,},
+    { id: 3, text: "Fantastic service! I felt comfortable and well-informed throughout.", author: "Emily Johnson" , imageURl : image3,},
+    { id:4, text: "Six Month with Fix Health was less than 1 month of my insurance co-pay", author:"Nasir Kamal", imageURl :image4,}
   ];
 
   return (
@@ -17,6 +23,7 @@ function Testimonials() {
       
       {testimonials.map(testimonial => (
         <div key={testimonial.id} className="testimonial">
+          <img src={testimonial.imageURl} alt={testimonial.author} className="testimonial-image" />
           <p>{testimonial.text}</p>
           <span>- {testimonial.author}</span>
         </div>
